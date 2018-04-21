eb2cae1b92ae43475d8fccbd91fc08ccf3e5eb16
Essim <essimcaldin@gmail.com>
2018-04-21 07:53:04 +0200

    added mock nurses to inject

--- src/server/injectMongo.js
+++ src/server/injectMongo.js
@@ -38,7 +38,7 @@ const inf2 = new Infirmier({
   sexe: FEMALE,
   languages: ['Néerlandais', 'Français'],
   email: 'sacre.christopher@fake.mail',
-  phone: '04729275',
+  phone: '04729276',
   description: 'cookie',
   zone: ['Woluwe', 'Ixelles'],
   postCodes: ['1050', '1200'],
@@ -52,5 +52,47 @@ const inf2 = new Infirmier({
   index: true
 });
 
+const inf3 = new Infirmier({
+  lastName: 'John',
+  firstName: 'Doe',
+  sexe: FEMALE,
+  languages: ['Néerlandais'],
+  email: 'sacre2.christopher@fake.mail',
+  phone: '04729277',
+  description: 'cookie',
+  zone: ['Woluwe'],
+  postCodes: ['1200'],
+  specificity: 'Oncologie',
+  availability: {
+    dayTimes: ['Matin', 'Soir'],
+    weekTimes: ['Jeudi', 'Dimanche']
+  },
+  logicalDelete: false,
+  cuid: 'John1',
+  index: true
+});
+
+const inf4 = new Infirmier({
+  lastName: 'John2',
+  firstName: 'Doe2',
+  sexe: MALE,
+  languages: ['Français'],
+  email: 'sacre3.christopher@fake.mail',
+  phone: '04729278',
+  description: 'cookie',
+  zone: ['Ixelles'],
+  postCodes: ['1110'],
+  specificity: 'Oncologie',
+  availability: {
+    dayTimes: ['Midi', 'Soir'],
+    weekTimes: ['Vendredi', 'Samedi', 'Dimanche']
+  },
+  logicalDelete: false,
+  cuid: 'John2',
+  index: true
+});
+
 inf1.save();
 inf2.save();
+inf3.save();
+inf4.save();
